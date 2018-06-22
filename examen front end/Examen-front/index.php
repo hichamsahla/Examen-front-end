bcf69942743bff666984565b5bdc7f1031ad2cdc
hichamsahla <23242@ma-web.nl>
2018-06-22 09:02:53 +0200

    Add files via upload

---
+++ examen front end/Examen-front/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+      <link rel="stylesheet" href="css/style.css"/>
+    <meta charset="utf-8">
+    <title>Ma-Web</title>
+  </head>
+  <body>
+    <div class="grid">
+
+
+        <div class="nav">
+
+<img class="logo"  src="img/logo_ma.jpg">
+        
+          <div class="topnav">
+   <a class="active" href="#home">Home</a>
+   <a href="#news">News</a>
+   <a href="#contact">Contact</a>
+   <a href="#about">About</a>
+ </div>
+</div>
+        <div class="content">
+        </div>
+        <div footer>
+        </div>
+
+            </div>
+
+  </body>
+</html>
